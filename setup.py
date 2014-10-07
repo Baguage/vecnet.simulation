9cf14dd6d6e0d7fd6987da32602659911e180e85
Jimm Domingo <jdomingo@green-code.com>
2014-10-07 18:42:51 -0500

    The contents of the README is now used as the package's long description, which is shown automatically on the package's PyPI page.

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import find_packages, setup
 
+def read_file(name):
+    with open(name, 'r') as f:
+        return f.read()
+
 setup(
     name="vecnet.simulation",
     version="0.1",
     description="Library for simulation models in VecNet",
+    long_description=read_file('README.rst'),
     author='Center for Research Computing, University of Notre Dame',
     packages=find_packages(),
     namespace_packages=['vecnet', ],
