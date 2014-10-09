dc180c3e66d2146ba31cfce29e367eb4af1de48a
Jimm Domingo <jdomingo@green-code.com>
2014-10-09 15:08:42 -0500

    Added more metadata fields so this package is more inline with other VecNet packages.

--- setup.py
+++ setup.py
@@ -7,9 +7,22 @@ def read_file(name):
 setup(
     name="vecnet.simulation",
     version="0.1",
+
+    # Metadata for PyPI
     description="Library for simulation models in VecNet",
     long_description=read_file('README.rst'),
-    author='Center for Research Computing, University of Notre Dame',
+    author="VecNet Team, Center for Research Computing, University of Notre Dame",
+    author_email="dev@vecnet.org",
+    url="https://github.com/jimm-domingo/vecnet.simulation",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
     packages=find_packages(),
     namespace_packages=['vecnet', ],
 )
